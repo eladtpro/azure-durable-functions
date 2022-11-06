8122c09655b7686fddd858b7c0e6594a25b37ef8
eladtpro <eladt.pro@gmail.com>
2022-11-06 14:03:49 +0200

    add simple BlobTrigger

---
+++ Functions/BlobTrigger.cs
@@ -0,0 +1,23 @@
+namespace ImageIngest.Functions;
+
+public class BlobTrigger
+{
+    [FunctionName(nameof(BlobTrigger))]
+    public async Task Run(
+        [BlobTrigger("images/{name}", Source = BlobTriggerSource.EventGrid, Connection = "AzureWebJobsFTPStorage")] BlobClient blobClient,
+        [DurableClient] IDurableOrchestrationClient starter,
+        ILogger log)
+    {
+        log.LogInformation($"[BlobTrigger] Function triggered blob name: {blobClient.Name}");
+        log.LogInformation($"[BlobTrigger] BlobClient Blob: {blobClient.Name}, Container: {blobClient.BlobContainerName}, AccountName: {blobClient.AccountName}");
+        BlobProperties props = await blobClient.GetPropertiesAsync();
+        log.LogInformation($"[BlobTrigger] BlobProperties: {props}");
+        BlobTags tags = new BlobTags(props, blobClient);
+        ActivityAction activity = new ActivityAction(tags);
+        Response response = await blobClient.WriteTagsAsync(tags);
+        if(response.IsError)
+            log.LogError(new EventId(1001), response.ToString());
+        log.LogInformation($"[BlobTrigger] BlobTags saved for blob {blobClient.Name}, Tags: {tags}");
+        await starter.StartNewAsync<ActivityAction>(nameof(Orchestrator), activity);
+    }
+}
